c6c83dc33a1f69562a7b3cd3e0b8383ce9290278
Бордюжа Марк <borduzamark@gmail.com>
2025-09-09 08:52:19 +0300

    Add files via upload

---
+++ second-task.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondTaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 3.5;
+            Console.WriteLine($"Сума: {Add(a, (int)b)}");
+            Console.WriteLine($"Множення: {Multiply(a, b)}");
+            
+            if (a > b && b > 0)
+            {
+                Console.WriteLine("a більше b і b більше 0");
+            }
+            else if (a == b || b < 0)
+            {
+                Console.WriteLine("a дорівнює b або b менше 0");
+            }
+            else
+            {
+                Console.WriteLine("Жодна умова не виконана");
+            }
+            
+            string result = (a > b) ? "a більше b" : "a менше або дорівнює b";
+            Console.WriteLine(result);
+            
+            int[] numbers = {1, 2, 3, 4, 5};
+            Console.WriteLine("for");
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write(numbers[i] + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("foreach");
+            foreach (var num in numbers)
+                Console.Write(num + " ");
+            Console.WriteLine();
+            
+            List<string> names = new List<string>() {"Tom", "Bob", "Sam"};
+            int index = 0;
+            Console.WriteLine("while");
+            while (index < names.Count)
+            {
+                Console.Write(names[index] + " ");
+                index++;
+            }
+            Console.WriteLine();
+
+            index = 0;
+            Console.WriteLine("do-while");
+            do
+            {
+                Console.Write(names[index] + " ");
+                index++;
+            } while (index < names.Count);
+            Console.WriteLine();
+        }
+        
+        static int Add(int x, int y) => x + y;
+        static double Multiply(double x, double y) => x * y;
+    }
+}
---
+++ second-task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MyClass
+{
+    
+    private string name;
+
+    
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    
+    public MyClass()
+    {
+        name = "Constructor";
+    }
+
+    
+    public MyClass(string name)
+    {
+        this.name = name;
+    }
+
+    
+    public void PublicMethod()
+    {
+        Console.WriteLine($"Публічний метод. Name = {name}");
+        PrivateMethod();
+    }
+    
+    private void PrivateMethod()
+    {
+        Console.WriteLine("Це приватний метод");
+    }
+}
