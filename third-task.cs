37af9ad5f95f8ca6643143988a2bd1fb2341933d
Бордюжа Марк <borduzamark@gmail.com>
2025-09-12 12:15:38 +0300

    Add files via upload

---
+++ third-task.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+interface IGenericInterface<T>
+{
+    void GenericMethod(T value);
+}
+
+interface ISimpleInterface
+{
+    void SimpleMethod();
+}
+
+abstract class MyAbstractClass
+{
+    public abstract void AbstractMethod();
+
+    public void NormalMethod()
+    {
+        Console.WriteLine("Звичайний метод з абстрактного класу");
+    }
+}
+
+class MyClass : MyAbstractClass, IGenericInterface<int>, ISimpleInterface
+{
+    public override void AbstractMethod()
+    {
+        Console.WriteLine("Реалізація абстрактного методу");
+    }
+
+    public void GenericMethod(int value)
+    {
+        Console.WriteLine($"Generic метод отримав: {value}");
+    }
+
+    public void SimpleMethod()
+    {
+        Console.WriteLine("Простий метод виконано");
+    }
+}
---
+++ third-task2.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        MyClass obj = new MyClass();
+       
+
+        await RunTasks();
+        await RunTasksReturnFirst();
+    }
+
+    static async Task RunTasks()
+    {
+        Random rnd = new Random();
+        Task[] tasks = new Task[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            int taskNum = i + 1;
+            tasks[i] = Task.Run(async () =>
+            {
+                await Task.Delay(rnd.Next(1000, 3000));
+                Console.WriteLine($"Task {taskNum}");
+            });
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    static async Task RunTasksReturnFirst()
+    {
+        Random rnd = new Random();
+        Task<string>[] tasks = new Task<string>[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            int taskNum = i + 1;
+            tasks[i] = Task.Run(async () =>
+            {
+                await Task.Delay(rnd.Next(1000, 3000));
+                return $"Task {taskNum}";
+            });
+        }
+
+        var first = await Task.WhenAny(tasks);
+        Console.WriteLine($"Першим завершився: {await first}");
+    }
+}
+
+internal class MyClass
+{
+}
