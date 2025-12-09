1aa297f3641e3563778066ecffa21b921ce9c6c1
Бордюжа Марк <borduzamark@gmail.com>
2025-12-09 20:58:37 +0200

    Add files via upload

---
+++ First simple DB/First simple DB/Data.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DbOperationsApp.Models;
+
+namespace DbOperationsApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Person> People => Set<Person>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql("Host=localhost;Port=5432;Database=mydb;Username=myuser;Password=mypassword");
+    }
+}
